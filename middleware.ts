e02c2901a0bad6d35a0df09080b52a3865e60182
sorahiraoka <sorahiraoka787@gmail.com>
2026-05-01 16:00:07 +0900

    disable middleware

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,20 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
-  const token =
-    req.cookies.get("sb-access-token") ||
-    req.cookies.get("supabase-auth-token");
+// export function middleware(req: NextRequest) {
+//   const token =
+//     req.cookies.get("sb-access-token") ||
+//     req.cookies.get("supabase-auth-token");
 
-  const path = req.nextUrl.pathname;
+//   const path = req.nextUrl.pathname;
 
-  // 管理者・講師ページ保護
-  if (
-    (path.startsWith("/admin") ||
-      path.startsWith("/teacher")) &&
-    !token
-  ) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
+//   if (
+//     (path.startsWith("/admin") ||
+//       path.startsWith("/teacher")) &&
+//     !token
+//   ) {
+//     return NextResponse.redirect(new URL("/", req.url));
+//   }
 
-  return NextResponse.next();
-}
-
-export const config = {
-  matcher: ["/admin/:path*", "/teacher/:path*"],
-};
+//   return NextResponse.next();
+// }
